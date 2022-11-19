7acfd7d97996c00e4c4b7637a896d604fded433f
Rahul Yadav <rahulkyadav57@gmail.com>
2022-11-19 11:07:33 +0530

    Cleanup import

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,4 +1,1 @@
-export { default as DashboardLayout } from "./DashboardLayout";
-export { default as DashboardSidebar } from "./DashboardSidebar";
-export { default as Footer } from "./Footer";
 export { default as GuestLayout } from "./Layout";
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,5 +1,3 @@
 export { default as AppLogo } from "./AppLogo";
 export { default as Loader } from "./Loader";
-export { default as OurSocial } from "./OurSocial";
-export { default as ProgressBar } from "./ProgressBar";
 export { default as Skeleton } from "./Skeleton";
